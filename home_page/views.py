ff31577eada74fb4b78bdc7f19ec57340b525119
drra1nee <semen.sabetov@yandex.ru>
2025-11-20 23:01:22 +0300

    счетчик проблем

--- home_page/views.py
+++ home_page/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from issues.models import Issue
 
 def home_view(request):
-    return render(request, 'home_page/home.html')
+    issues_in_progress = Issue.objects.filter(status=Issue.STATUS_IN_PROGRESS).count()
+
+    issues_resolved = Issue.objects.filter(status=Issue.STATUS_RESOLVED).count()
+    
+    context = {
+        'issues_in_progress': issues_in_progress,
+        'issues_resolved': issues_resolved,
+    }
+    
+    return render(request, 'home_page/home.html', context)
+
 def about_site(request):
     return render(request, 'about_site.html')
